676e3d6c978d7a1d6767f721379c9f1381aad8b5
Alexander Sotskii <64553081+Alexanderwebsot@users.noreply.github.com>
2022-04-09 17:05:53 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -6,6 +6,31 @@ $(document).ready(function () {
     $('body,html').animate({scrollTop: top}, 1500);
   });
 
+  $('.modal-open').on('click', function() {
+    $('.modal-window-form').addClass('modal-window-active');
+    $('.dark-window').addClass('modal-window-active');
+    return false;
+  })
+
+  $('.dark-window').on('click', function() {
+    $('.modal-window').removeClass('modal-window-active');
+    $('.dark-window').removeClass('modal-window-active');
+  })
+
+  $('.modal-window__close').on('click', function() {
+    $('.modal-window').removeClass('modal-window-active');
+    $('.dark-window').removeClass('modal-window-active');
+  })
+
+  $('.nav-btn').on('click', function() {
+    $('.modal-window__title_change').text('заказа звонка');
+    $('.modal-window__btn').text('заказать звонок');
+  })
+  $('.sinlge-product-btn').on('click', function() {
+    $('.modal-window__title_change').text('оформления заказа');
+    $('.modal-window__btn').text('оформить заказ');
+  })
+
   $('.reviews-slider').slick({
       infinite: false,
       slidesToShow: 1,
@@ -18,7 +43,61 @@ $(document).ready(function () {
       nextArrow: $('.reviews-slider-arrow__next'),
    });
 
+  $('.sinlge-product-navbar-slider').slick({
+      infinite: true,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      adaptiveHeight: true,
+      vertical: true,
+      swipe: false,
+      //arrows: true,
+      prevArrow: $('.sinlge-product-arrow_prev'),
+      nextArrow: $('.sinlge-product-arrow_next'),
+      asNavFor: '.sinlge-product-big__slider',
+      focusOnSelect: true,
+   });
+  $('.sinlge-product-big__slider').slick({
+      asNavFor: '.sinlge-product-navbar-slider',
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipe: true,
+      arrows: false,
+   });
 
+  $('.projects-block-slider').slick({
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipe: false,
+      arrows: false,
+   });
+
+  $('.projects-about-slider').slick({
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipe: true,
+      arrows: true,
+      speed: 700,
+      fade: true,
+      prevArrow: $('.projects-slider-arrow__prev'),
+      nextArrow: $('.projects-slider-arrow__next'),
+   });
+
+  $('.projects-block-navslide').on('click', function() {
+    let parent = $(this).closest('.projects-block');
+    let slider_parent = $(parent).children('.projects-block-slider');
+    let all_slides = $(parent).children('.projects-block-navslider');
+    all_slides = $(all_slides).children('.projects-block-navslide');
+    let index_slide = $(all_slides).index(this);
+    $(slider_parent).slick('slickGoTo', index_slide);
+  })
+
+  $('.catalog-catagory__item__arrow').on('click', function() {
+    let parent = $(this).closest('.catalog-catagory__item');
+    $(parent).toggleClass('catalog-catagory__item_active');
+  })
  
   let reviews_count = $('.reviews-slide').length;
   $('.counter_all').text(reviews_count);
